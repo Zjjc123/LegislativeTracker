884b46cf6a294011ed3c548eb43f0dbf56327334
Jason Zhang <zjjc123@hotmail.com>
2020-07-23 13:55:21 -0700

    Basic Search

--- components/bill_list/BillListView.tsx
+++ components/bill_list/BillListView.tsx
@@ -1,37 +1,67 @@
 import React, { useState, useEffect } from 'react';
-import { View, FlatList, Text, SafeAreaView, StatusBar, StyleSheet, Button, Alert } from 'react-native';
-import { ListItem } from 'react-native-elements'
-var url = 'https://api.propublica.org/congress/v1/115/house/bills/introduced.json'
+import { View, FlatList, Text, SafeAreaView, StatusBar, StyleSheet, Button, Alert, Dimensions } from 'react-native';
+import { ListItem, SearchBar } from 'react-native-elements'
+// var url = 'https://api.propublica.org/congress/v1/116/house/bills/introduced.json'
+var url = 'https://jsonplaceholder.typicode.com/todos'
 var apiKey = "DvH6rYhOGmII5UmALNyzXGnRS92InKR3ymkTsP9CDvH6rYhOGmII5UmALNyzXGnRS92InKR3ymkTsP9C"
 
 export default function BillListView() {
   const [bills, setBills] = useState()
+  const [initialBills, setInitialBills] = useState()
+  const [searchValue, setSearchValue] = useState()
 
   // Empty dependency array in use effect (runs once)
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
+    fetch(url, {
+      headers: {
+        "X-API-Key": apiKey
+      }
+    })
       .then(response => response.json())
       .then(json =>
         // Triggers rerender
-        setBills(json)
+        initializeBill(json)
       )
-    console.log(bills)
   }, [])
 
-  const Item = ({ title }) => (
+  const initializeBill = (json) => {
+    setBills(json)
+    setInitialBills(json)
+  }
+
+  const Item = ({ title, bill_id }) => (
     <View>
-      <ListItem style = {styles.listItem}
+      <ListItem
         title = {title}
+        subtitle = {bill_id}
         onPress={() => Alert.alert('Simple Button pressed')}/>
     </View>
   );
 
   const renderItem = ({ item }) => (
-    <Item title={item.title} />
+    <Item title={item.title} bill_id = {item.userId} />
   );
+
+  const searchBills = (search) => {
+    const filteredBills = initialBills.filter(
+      bill => {
+        let billLowerCase = bill.title.toLowerCase()
+        let searchTermLowerCase = search.toLowerCase()
+
+        return billLowerCase.indexOf(searchTermLowerCase) > -1
+      }
+    )
+    setBills(filteredBills)
+    setSearchValue(search)
+  }
 
   return (
     <SafeAreaView style={styles.container}>
+      <SearchBar
+        placeholder = "Search"
+        value = {searchValue}
+        onChangeText = {(value) => searchBills(value)}
+      />
       <FlatList
         data={bills}
         renderItem={renderItem}
@@ -45,15 +75,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'flex-start',
+    width: Dimensions.get('screen').width
   },
   title: {
     textAlign: 'center',
     marginVertical: 8,
-  },
-  listItem: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'stretch',
   },
 });
 
