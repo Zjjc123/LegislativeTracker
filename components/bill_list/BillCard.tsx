b6c16e02b9689ac6f8d5798dfb1ee1d01a81f1d5
Jason Zhang <zjjc123@hotmail.com>
2020-07-23 21:59:59 -0700

    Size Update

--- components/bill_list/BillCard.tsx
+++ components/bill_list/BillCard.tsx
@@ -9,63 +9,83 @@ export default function BillCard(props) {
         switch (props.committee) {
             case "House Agriculture Committee":
                 colorHex = "#ff0000"
+                com = "Agriculture"
                 break
             case "House Appropriations Committee":
                 colorHex = "#ff8c00"
+                com = "Appropriations"
                 break
             case "House Armed Services Committee":
                 colorHex = "#F7CB15"
+                com = "Armed Services"
                 break
             case "House Budget Committee":
                 colorHex = "#7cfc00"
+                com = "Budget"
                 break
             case "House Education and Labor Committee":
                 colorHex = "#ba55d3"
+                com = "Education and Labor"
                 break
             case "House Energy and Commerce Committee":
                 colorHex = "#00fa9a"
+                com = "Energy and Commerce"
                 break
             case "House Ethics Committee":
                 colorHex = "#ff00ff"
+                com = "Ethics"
                 break
             case "House Financial Services Committee":
                 colorHex = "#0000ff"
+                com = "Financial Services"
                 break
             case "House Foreign Affairs Committee":
                 colorHex = "#f08080"
+                com = "Foreign Affairs"
                 break
             case "House Homeland Security Committee":
                 colorHex = "#1e90ff"
+                com = "Homeland Security"
                 break
             case "House Committee on House Administration":
                 colorHex = "#d3d3d3"
+                com = "House Administration"
                 break
             case "House Judiciary Committee":
                 colorHex = "#2f4f4f"
+                com = "Judiciary"
                 break
             case "House Natural Resources Committee":
                 colorHex = "#2e8b57"
+                com = "Natural Resources"
                 break
             case "House Oversight and Reform Committee":
                 colorHex = "#7f0000"
+                com = "Oversight and Reform"
                 break
             case "House Rules Committee":
                 colorHex = "#808000"
+                com = "Rules"
                 break
             case "House Science, Space, and Technology Committee":
                 colorHex = "#00008b"
+                com = "Science, Space, and Technology"
                 break
             case "House Small Business Committee":
                 colorHex = "#f0e68c"
+                com = "Small Business"
                 break
             case "House Transportation and Infrastructure Committee":
                 colorHex = "#160F29"
+                com = "Transportation and Infrastructure"
                 break
             case "House Veteran's Affairs Committee":
                 colorHex = "#FE5F55"
+                com = "Veteran's Affairs"
                 break
             case "House Ways and Means Committee":
                 colorHex = "#4F5D2F"
+                com = "Ways and Means"
                 break
             case "":
                 com = "None"
@@ -76,11 +96,14 @@ export default function BillCard(props) {
             <Card containerStyle={{
                 backgroundColor: colorHex,
                 borderRadius: 10,
+                justifyContent: "center",
                 flex: 1
             }}>
                 <Text style={{
                     color: "#ffffff",
-                    textAlign: "center"
+                    textAlign: "center",
+                    textAlignVertical: "center",
+
                 }}>{com}</Text>
             </Card>
         )
@@ -91,11 +114,13 @@ export default function BillCard(props) {
             <Card containerStyle={{
                 backgroundColor: "#000000",
                 borderRadius: 10,
+                justifyContent: "center",
                 flex: 1
             }}>
                 <Text style={{
                     color: "#ffffff",
-                    textAlign: "center"
+                    textAlign: "center",
+                    textAlignVertical: "center",
                 }}>{props.name}</Text>
             </Card>
         )
